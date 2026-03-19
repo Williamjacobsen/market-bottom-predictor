6e7ebd02d5c8c1b09aa9a52b1ec55b8936560df2
username <villi05.v.j@gmail.com>
2026-03-19 18:54:42 +0100

    woops, found a data problem, will refactor later

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,16 @@ use std::{
     io::{Error as IoError, ErrorKind},
 };
 
-fn read_data_set() -> Result<(Vec<Vec<f64>>, Vec<Vec<f64>>), Box<dyn Error>> {
+fn min_max_normalization(input: &mut Vec<f64>) {
+    let min = input.iter().fold(f64::INFINITY, |a, &b| f64::min(a, b));
+    let max = input.iter().fold(f64::NEG_INFINITY, |a, &b| f64::max(a, b));
+
+    for x in input.iter_mut() {
+        *x = (*x - min) / (max - min);
+    }
+}
+
+fn get_data() -> Result<((Vec<Vec<f64>>, Vec<f64>), (Vec<Vec<f64>>, Vec<f64>)), Box<dyn Error>> {
     let file = File::open("training_data.csv")?;
     let mut rdr = csv::Reader::from_reader(file);
 
@@ -27,12 +36,37 @@ fn read_data_set() -> Result<(Vec<Vec<f64>>, Vec<Vec<f64>>), Box<dyn Error>> {
         records.push(row);
     }
 
-    let (training_data, evaluation_data) = records.split_at((records.len() as f64 * 0.8) as usize);
+    let split_idx = (records.len() as f64 * 0.8) as usize;
+    let (training_records, evaluation_records) = records.split_at(split_idx);
 
-    Ok((training_data.to_vec(), evaluation_data.to_vec()))
+    let (training_inputs, training_labels): (Vec<Vec<f64>>, Vec<f64>) = training_records
+        .iter()
+        .map(|record| {
+            let (input, label) = record.split_at(100);
+            let mut input = input.to_vec();
+            min_max_normalization(&mut input);
+            (input.to_vec(), label[0])
+        })
+        .unzip();
+
+    let (evaluation_inputs, evaluation_labels): (Vec<Vec<f64>>, Vec<f64>) = evaluation_records
+        .iter()
+        .map(|record| {
+            let (input, label) = record.split_at(100);
+            let mut input = input.to_vec();
+            min_max_normalization(&mut input);
+            (input.to_vec(), label[0])
+        })
+        .unzip();
+
+    Ok((
+        (training_inputs, training_labels),
+        (evaluation_inputs, evaluation_labels),
+    ))
 }
 
 fn main() {
-    let (training_data, evaluation_data) = read_data_set().unwrap();
-    println!("{:?}", training_data);
+    let ((training_inputs, training_labels), (evaluation_inputs, evaluation_labels)) =
+        get_data().unwrap();
+    println!("{:?}", training_inputs);
 }
